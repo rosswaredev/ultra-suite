2a34ab7cebffc82f7d9fda0702effc6f7e33b39b
Ross Ware <rosswaredev@gmail.com>
2023-03-21 13:38:49 +1000

    simplify tab bar icon config

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,5 +1,11 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { ComponentProps } from "react";
+
+const tabBarIcon =
+  (name: ComponentProps<typeof MaterialCommunityIcons>["name"]) =>
+  ({ size, color }) =>
+    <MaterialCommunityIcons name={name} size={size} color={color} />;
 
 export default function Home() {
   return (
@@ -8,51 +14,19 @@ export default function Home() {
     >
       <Tabs.Screen
         name="tasks"
-        options={{
-          title: "Tasks",
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons name="check" size={size} color={color} />
-          ),
-        }}
+        options={{ title: "Tasks", tabBarIcon: tabBarIcon("check") }}
       />
       <Tabs.Screen
         name="habits/index"
-        options={{
-          title: "Habits",
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons
-              name="playlist-check"
-              size={size}
-              color={color}
-            />
-          ),
-        }}
+        options={{ title: "Habits", tabBarIcon: tabBarIcon("playlist-check") }}
       />
       <Tabs.Screen
         name="flows/index"
-        options={{
-          title: "Flows",
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons
-              name="playlist-play"
-              size={size}
-              color={color}
-            />
-          ),
-        }}
+        options={{ title: "Flows", tabBarIcon: tabBarIcon("playlist-play") }}
       />
       <Tabs.Screen
         name="calendar/index"
-        options={{
-          title: "Calendar",
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons
-              name="calendar-today"
-              size={size}
-              color={color}
-            />
-          ),
-        }}
+        options={{ title: "Calendar", tabBarIcon: tabBarIcon("calendar") }}
       />
     </Tabs>
   );
