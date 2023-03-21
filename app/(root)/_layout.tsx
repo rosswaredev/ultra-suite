4904b2aa47c552de8c66c7c03cb12a5fe8a58715
Ross Ware <rosswaredev@gmail.com>
2023-03-21 13:35:28 +1000

    add tab bar icons

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,12 +1,59 @@
-import { Stack, Tabs } from 'expo-router';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function Home() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="tasks" options={{ title: 'Tasks' }} />
-      <Tabs.Screen name="habits/index" options={{ title: 'Habits' }} />
-      <Tabs.Screen name="flows/index" options={{ title: 'Flows' }} />
-      <Tabs.Screen name="calendar/index" options={{ title: 'Calendar' }} />
+    <Tabs
+      screenOptions={{ headerShown: false, tabBarStyle: { paddingBottom: 3 } }}
+    >
+      <Tabs.Screen
+        name="tasks"
+        options={{
+          title: "Tasks",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="check" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="habits/index"
+        options={{
+          title: "Habits",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="playlist-check"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="flows/index"
+        options={{
+          title: "Flows",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="playlist-play"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calendar/index"
+        options={{
+          title: "Calendar",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="calendar-today"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
