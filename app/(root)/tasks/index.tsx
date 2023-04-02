fd3c704fd2c57116ee09717e4e5cd0343708dfd8
Ross Ware <rosswaredev@gmail.com>
2023-04-02 15:25:28 +1000

    env var support

--- app/(root)/tasks/index.tsx
+++ app/(root)/tasks/index.tsx
@@ -5,7 +5,7 @@ export default function TasksHome() {
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic">
       <Stack.Screen options={{ title: "Tasks" }} />
-      <Text>Tasks Home</Text>
+      <Text className="text-base-content">{process.env.POCKETBASE_URL}</Text>
     </ScrollView>
   );
 }
--- src/features/sync/pocket-base/pocket-base.ts
+++ src/features/sync/pocket-base/pocket-base.ts
@@ -4,5 +4,5 @@ import eventsource from "react-native-sse";
 // @ts-ignore
 global.EventSource = eventsource;
 
-const url = "http://127.0.0.1:8090";
+const url = process.env.POCKETBASE_URL ?? "http://127.0.0.1:8090";
 export const pocketBaseClient = new PocketBase(url);
