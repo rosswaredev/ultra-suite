8df651c982186bebe80b6f92fc85ff035faddadb
Ross Ware <rosswaredev@gmail.com>
2023-04-05 20:26:02 +1000

    update tab icons

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,12 +1,12 @@
-import { Octicons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
-import { ComponentProps } from 'react';
-import colors from '../../src/theme/colors.json';
+import { Octicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { ComponentProps } from "react";
+import colors from "../../src/theme/colors.json";
 
 const tabBarIcon =
-  (name: ComponentProps<typeof Octicons>['name']) =>
+  (name: ComponentProps<typeof Octicons>["name"]) =>
   ({ size, color }) =>
-    <Octicons name={name} size={size} color={color} />;
+    <Octicons name={name} size={size - 2} color={color} />;
 
 export default function Home() {
   return (
@@ -14,34 +14,34 @@ export default function Home() {
       <Tabs
         screenOptions={{
           headerShown: false,
-          tabBarInactiveTintColor: colors['base-400'],
+          tabBarInactiveTintColor: colors["base-400"],
         }}
       >
         <Tabs.Screen
           name="tasks"
           options={{
-            title: 'Tasks',
-            tabBarIcon: tabBarIcon('check'),
+            title: "Tasks",
+            tabBarIcon: tabBarIcon("check"),
           }}
         />
         <Tabs.Screen
           name="habits"
           options={{
-            title: 'Habits',
-            tabBarIcon: tabBarIcon('graph'),
+            title: "Habits",
+            tabBarIcon: tabBarIcon("graph"),
           }}
         />
         <Tabs.Screen
           name="flows/index"
-          options={{ title: 'Flows', tabBarIcon: tabBarIcon('list-ordered') }}
+          options={{ title: "Flows", tabBarIcon: tabBarIcon("list-unordered") }}
         />
         <Tabs.Screen
           name="calendar/index"
-          options={{ title: 'Calendar', tabBarIcon: tabBarIcon('calendar') }}
+          options={{ title: "Calendar", tabBarIcon: tabBarIcon("calendar") }}
         />
         <Tabs.Screen
           name="settings"
-          options={{ title: 'Settings', tabBarIcon: tabBarIcon('gear') }}
+          options={{ title: "Settings", tabBarIcon: tabBarIcon("gear") }}
         />
       </Tabs>
     </>
