4f22bcc9f8057c0c26f105e058508de25d08fc8d
Ross Ware <rosswaredev@gmail.com>
2023-04-11 14:59:33 +1000

    use lucide icons

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,12 +1,29 @@
-import { Octicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { ComponentProps } from "react";
 import colors from "../../src/theme/colors.json";
 
+import {
+  Calendar,
+  Check,
+  ListVideo,
+  Settings,
+  TrendingUp,
+} from "lucide-react-native";
+
+const TAB_BAR_ICON_MAP = {
+  check: Check,
+  "trending-up": TrendingUp,
+  "list-video": ListVideo,
+  calendar: Calendar,
+  settings: Settings,
+};
+type IconName = keyof typeof TAB_BAR_ICON_MAP;
+
 const tabBarIcon =
-  (name: ComponentProps<typeof Octicons>["name"]) =>
-  ({ size, color }) =>
-    <Octicons name={name} size={size - 2} color={color} />;
+  (name: IconName) =>
+  ({ size, color }) => {
+    const Icon = TAB_BAR_ICON_MAP[name];
+    return <Icon size={size - 2} color={color} />;
+  };
 
 export default function Home() {
   return (
@@ -28,12 +45,12 @@ export default function Home() {
           name="habits"
           options={{
             title: "Habits",
-            tabBarIcon: tabBarIcon("graph"),
+            tabBarIcon: tabBarIcon("trending-up"),
           }}
         />
         <Tabs.Screen
           name="flows/index"
-          options={{ title: "Flows", tabBarIcon: tabBarIcon("list-unordered") }}
+          options={{ title: "Flows", tabBarIcon: tabBarIcon("list-video") }}
         />
         <Tabs.Screen
           name="calendar/index"
@@ -41,7 +58,7 @@ export default function Home() {
         />
         <Tabs.Screen
           name="settings"
-          options={{ title: "Settings", tabBarIcon: tabBarIcon("gear") }}
+          options={{ title: "Settings", tabBarIcon: tabBarIcon("settings") }}
         />
       </Tabs>
     </>
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,35 +1,35 @@
-import { Octicons } from '@expo/vector-icons';
-import { default as cn } from 'classnames';
-import { TouchableOpacity, View } from 'react-native';
-import { slop } from '../utils/slop';
+import { default as cn } from "classnames";
+import { TouchableOpacity, View } from "react-native";
+import { slop } from "../utils/slop";
 
-import colors from '../theme/colors.json';
+import { Check } from "lucide-react-native";
+import colors from "../theme/colors.json";
 
 export type CheckboxProps = {
   isChecked: boolean;
   onToggle: () => void;
-  size?: 'md' | 'lg';
+  size?: "md" | "lg";
 };
 
 export const Checkbox = ({
   isChecked,
   onToggle,
-  size = 'md',
+  size = "md",
 }: CheckboxProps) => (
   <TouchableOpacity onPress={onToggle} hitSlop={slop.all(20)}>
     <View
       className={cn(
-        'border-2 rounded-full justify-center items-center',
-        size === 'md' ? 'w-6 h-6' : 'w-8 h-8',
-        isChecked ? 'bg-primary-base' : 'bg-base-200',
-        isChecked ? 'border-primary-base' : 'border-primary-base/50'
+        "border-2 rounded-full justify-center items-center",
+        size === "md" ? "w-6 h-6" : "w-8 h-8",
+        isChecked ? "bg-primary-base" : "bg-base-200",
+        isChecked ? "border-primary-base" : "border-primary-base/50"
       )}
     >
       {isChecked ? (
-        <Octicons
+        <Check
           name="check"
-          size={size === 'md' ? 16 : 20}
-          color={colors['primary-content']}
+          size={size === "md" ? 16 : 20}
+          color={colors["primary-content"]}
         />
       ) : null}
     </View>
