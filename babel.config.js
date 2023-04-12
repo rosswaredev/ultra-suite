fd361218e3fe19ccf3f246c5615bce90889a9fe0
Ross Ware <rosswaredev@gmail.com>
2023-04-12 17:30:20 +1000

    fix absolute imports

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,15 @@ module.exports = function (api) {
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            src: "./src",
+          },
+        },
+      ],
     ],
   };
 };
