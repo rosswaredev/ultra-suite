9939f19411bdbc5394b02a228d02e95fe94f5980
Ross Ware <rosswaredev@gmail.com>
2023-04-12 20:11:41 +1000

    attempt hard-coded expo-router entry

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import "expo-router/entry";
+import { registerRootComponent } from "expo";
+import { ExpoRoot } from "expo-router";
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context("./app");
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
 
 import "./src/init";
