ba253030f8c3ec6270d4f289307777a09224d88a
Ross Ware <rosswaredev@gmail.com>
2023-04-13 23:09:19 +1000

    attempt to fix layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
+import { Tabs } from "expo-router";
+import colors from "src/theme/colors.json";
+
+import { Icon, IconName } from "src/components/Icon";
+
 import {
-  ThemeProvider,
-  DefaultTheme,
   DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import colors from "src/theme/colors.json";
 
 const lightTheme: typeof DefaultTheme = {
   ...DefaultTheme,
@@ -28,15 +31,57 @@ const darkTheme: typeof DefaultTheme = {
   },
 };
 
-export default function App() {
+const tabBarIcon =
+  (name: IconName) =>
+  ({ size, color }) => {
+    return <Icon name={name} size={size - 2} color={color} />;
+  };
+
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "tasks",
+};
+
+export default function AppLayout() {
   const scheme = useColorScheme();
 
   const theme = scheme === "light" ? lightTheme : darkTheme;
-
   return (
     <ThemeProvider value={theme}>
       <SafeAreaProvider>
-        <Stack screenOptions={{ headerShown: false }} />
+        <Tabs
+          screenOptions={{
+            headerShown: false,
+            tabBarInactiveTintColor: colors["base-400"],
+          }}
+        >
+          <Tabs.Screen
+            name="tasks"
+            options={{
+              title: "Tasks",
+              tabBarIcon: tabBarIcon("check"),
+            }}
+          />
+          <Tabs.Screen
+            name="habits"
+            options={{
+              title: "Habits",
+              tabBarIcon: tabBarIcon("trending-up"),
+            }}
+          />
+          <Tabs.Screen
+            name="flows/index"
+            options={{ title: "Flows", tabBarIcon: tabBarIcon("list-video") }}
+          />
+          <Tabs.Screen
+            name="calendar/index"
+            options={{ title: "Calendar", tabBarIcon: tabBarIcon("calendar") }}
+          />
+          <Tabs.Screen
+            name="settings"
+            options={{ title: "Settings", tabBarIcon: tabBarIcon("settings") }}
+          />
+        </Tabs>
       </SafeAreaProvider>
     </ThemeProvider>
   );
