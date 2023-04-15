a5b0747a25fbc212e1a96113e7cb79dd6e2bbcc8
Ross Ware <rosswaredev@gmail.com>
2023-04-15 17:57:49 +1000

    enable data sync

--- src/init.ts
+++ src/init.ts
@@ -1,22 +1,22 @@
-import { setGlobalConfig } from "mobx-keystone";
-import { nanoid } from "nanoid";
-import { PocketBaseLoader } from "./features/sync/pocket-base/pocket-base-loader";
+import { setGlobalConfig } from 'mobx-keystone';
+import { nanoid } from 'nanoid';
+import { PocketBaseLoader } from './features/sync/pocket-base/pocket-base-loader';
 
-import "react-native-get-random-values";
-import { HabitStore } from "./features/habits/habit-store";
-import { loadActions } from "./features/sync/load-actions";
-import { persistActions } from "./features/sync/persist-actions";
-import { PocketBasePersister } from "./features/sync/pocket-base/pocket-base-persister";
-import { RootStore } from "./root-store";
-import { EventLog } from "./features/sync/event-log";
-import { TaskStore } from "./features/tasks/task-store";
+import 'react-native-get-random-values';
+import { HabitStore } from './features/habits/habit-store';
+import { loadActions } from './features/sync/load-actions';
+import { persistActions } from './features/sync/persist-actions';
+import { PocketBasePersister } from './features/sync/pocket-base/pocket-base-persister';
+import { RootStore } from './root-store';
+import { EventLog } from './features/sync/event-log';
+import { TaskStore } from './features/tasks/task-store';
 
-// setGlobalConfig({ modelIdGenerator: () => nanoid(15) });
+setGlobalConfig({ modelIdGenerator: () => nanoid(15) });
 
 const taskStore = new TaskStore({});
 const habitStore = new HabitStore({});
 export const rootStore = new RootStore({ taskStore, habitStore });
-// const eventLog = new EventLog({ version: 0 });
+const eventLog = new EventLog({ version: 0 });
 
-// persistActions(rootStore, eventLog, new PocketBasePersister());
-// loadActions(rootStore, eventLog, new PocketBaseLoader());
+persistActions(rootStore, eventLog, new PocketBasePersister());
+loadActions(rootStore, eventLog, new PocketBaseLoader());
