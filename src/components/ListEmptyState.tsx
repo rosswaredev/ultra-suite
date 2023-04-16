48aef7740ec5c4eac7a4c3a66618ec5760d80b16
Ross Ware <rosswaredev@gmail.com>
2023-04-16 15:45:47 +1000

    fix list empty state

--- src/components/ListEmptyState.tsx
+++ src/components/ListEmptyState.tsx
@@ -1,21 +1,21 @@
-import { View } from "react-native";
-import { Button, ButtonProps } from "./Button";
-import { Text } from "./Text";
-import { Icon, IconName } from "./Icon";
-import { tw } from "src/theme";
-import { capitalize } from "src/utils";
+import { View } from 'react-native';
+import { Button, ButtonProps } from './Button';
+import { Text } from './Text';
+import { Icon, IconName } from './Icon';
+import { tw } from 'src/theme';
+import { capitalize } from 'src/utils';
 
 type ListEmptyStateProps = {
   title: string;
   icon: IconName;
-} & Pick<ButtonProps, "onPress">;
+} & Pick<ButtonProps, 'onPress'>;
 export const ListEmptyState = ({
   title,
   icon,
   onPress,
 }: ListEmptyStateProps) => (
-  <View style={tw`justify-center items-center pt-10`}>
-    <Icon name={icon} size={64} color={tw.color("base-300")} />
+  <View style={tw`justify-center items-center flex-1`}>
+    <Icon name={icon} size={64} color={tw.color('base-300')} />
     <Text
       style={tw`mb-4 text-base-300`}
     >{`No ${title.toLowerCase()}s yet. Why not create one?`}</Text>
