f0910b3964b337a08e57cdaeac7092105306a4eb
Ross Ware <rosswaredev@gmail.com>
2023-04-17 12:13:47 +1000

    fix today link

--- src/features/tasks/view/components/TaskHomeLinks.tsx
+++ src/features/tasks/view/components/TaskHomeLinks.tsx
@@ -1,35 +1,35 @@
-import { useRouter } from 'expo-router';
-import { LinkProps } from 'expo-router/build/link/Link';
-import { Pressable } from 'react-native';
-import { Icon, IconName, Text } from 'src/components';
-import { tw } from 'src/theme';
-import { useTaskListPresenter } from '../hooks/useTaskListPresenter';
-import { observer } from 'mobx-react';
+import { useRouter } from "expo-router";
+import { LinkProps } from "expo-router/build/link/Link";
+import { Pressable } from "react-native";
+import { Icon, IconName, Text } from "src/components";
+import { tw } from "src/theme";
+import { useTaskListPresenter } from "../hooks/useTaskListPresenter";
+import { observer } from "mobx-react";
 
 const LINKS: TaskHomeLink[] = [
   {
-    list: 'inbox',
-    icon: 'inbox',
-    title: 'Inbox',
-    href: 'tasks/list?list=inbox',
+    list: "inbox",
+    icon: "inbox",
+    title: "Inbox",
+    href: "tasks/list?list=inbox",
   },
   {
-    list: 'completed',
-    icon: 'sun',
-    title: 'Today',
-    href: 'tasks/list?list=today',
+    list: "today",
+    icon: "sun",
+    title: "Today",
+    href: "tasks/list?list=today",
   },
   {
-    list: 'upcoming',
-    icon: 'calendar-plus',
-    title: 'Upcoming',
-    href: 'tasks/list?list=upcoming',
+    list: "upcoming",
+    icon: "calendar-plus",
+    title: "Upcoming",
+    href: "tasks/list?list=upcoming",
   },
   {
-    list: 'completed',
-    icon: 'check-circle',
-    title: 'Completed',
-    href: 'tasks/list?list=completed',
+    list: "completed",
+    icon: "check-circle",
+    title: "Completed",
+    href: "tasks/list?list=completed",
   },
 ];
 
@@ -53,12 +53,12 @@ export const TaskHomeLinks = observer(() => {
 export type TaskHomeLink = {
   icon: IconName;
   title: string;
-  href: LinkProps['href'];
-  list: 'inbox' | 'today' | 'upcoming' | 'completed';
+  href: LinkProps["href"];
+  list: "inbox" | "today" | "upcoming" | "completed";
 };
 type TaskHomeLinkProps = {
   count?: number;
-} & Omit<TaskHomeLink, 'list'>;
+} & Omit<TaskHomeLink, "list">;
 const TaskHomeLink = ({ icon, title, href, count }: TaskHomeLinkProps) => {
   const router = useRouter();
   const handlePress = () => router.push(href);
@@ -70,7 +70,7 @@ const TaskHomeLink = ({ icon, title, href, count }: TaskHomeLinkProps) => {
         tw.style(`flex-row items-center px-6 py-3`, pressed && `opacity-75`)
       }
     >
-      <Icon name={icon} size={24} color={tw.color('base-content')} />
+      <Icon name={icon} size={24} color={tw.color("base-content")} />
       <Text variant="heading" style={tw`ml-4 flex-1`}>
         {title}
       </Text>
